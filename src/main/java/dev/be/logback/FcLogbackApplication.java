d256cb8080def5819a541702824b65956dcde143
webdevyoo <yjk7454@naver.com>
2024-04-13 00:31:42 +0900

    Initial commit

---
+++ src/main/java/dev/be/logback/FcLogbackApplication.java
@@ -0,0 +1,13 @@
+package dev.be.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcLogbackApplication.class, args);
+    }
+
+}
